885be6d7dfcdeba80e3be9b2e43440185bd418fb
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2013-07-23 13:50:52 -0500

    Add getGeneticProfiles.

--- test/cgds-spec.js
+++ test/cgds-spec.js
@@ -44,4 +44,35 @@ describe("CDGS-JS", function () {
         });
     });
 
+    describe("getGeneticProfiles", function () {
+        var cgds;
+        beforeEach(function () {
+            cgds = new CGDS("http://www.cbioportal.org/public-portal/webservice.do");
+        });
+        it("should throw an error if a cancer_study_id parameter is not provided", function () {
+            expect(function () {
+                cgds.getGeneticProfiles();
+            }).to.throwException(function (e) {
+                expect(e).to.be.a(Error);
+                expect(e.message).to.be("Please provide a cancer_study_id parameter.");
+            });
+        });
+        it("should throw an error if a callback parameter is not provided", function () {
+            expect(function () {
+                cgds.getGeneticProfiles("brca_tcga");
+            }).to.throwException(function (e) {
+                expect(e).to.be.a(Error);
+                expect(e.message).to.be("Please provide a callback parameter.");
+            });
+        });
+        it("should return a JSON representation of a tab-delimited file with six columns", function (done) {
+            cgds.getGeneticProfiles("brca_tcga", function (err, res) {
+                expect(res).to.be.a(Array);
+                expect(res.length).to.be.greaterThan(0);
+                expect(Object.keys(res[0])).to.eql(["genetic_profile_id", "genetic_profile_name", "genetic_profile_description", "cancer_study_id", "genetic_alteration_type", "show_profile_in_analysis_tab"]);
+                done();
+            });
+        });
+    });
+
 });
