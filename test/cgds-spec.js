8da32e6272319be71ce4280c75f31d898bc3529f
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2013-07-23 13:56:31 -0500

    Add getCaseLists.

--- test/cgds-spec.js
+++ test/cgds-spec.js
@@ -98,4 +98,35 @@ describe("CDGS-JS", function () {
         });
     });
 
+    describe("getCaseLists", function () {
+        var cgds;
+        beforeEach(function () {
+            cgds = new CGDS("http://www.cbioportal.org/public-portal/webservice.do");
+        });
+        it("should throw an error if a cancer_study_id parameter is not provided", function () {
+            expect(function () {
+                cgds.getCaseLists();
+            }).to.throwException(function (e) {
+                expect(e).to.be.a(Error);
+                expect(e.message).to.be("Please provide a cancer_study_id parameter.");
+            });
+        });
+        it("should throw an error if a callback parameter is not provided", function () {
+            expect(function () {
+                cgds.getCaseLists("brca_tcga");
+            }).to.throwException(function (e) {
+                expect(e).to.be.a(Error);
+                expect(e.message).to.be("Please provide a callback parameter.");
+            });
+        });
+        it("should return a JSON representation of a tab-delimited file with five columns", function (done) {
+            cgds.getCaseLists("brca_tcga", function (err, res) {
+                expect(res).to.be.a(Array);
+                expect(res.length).to.be.greaterThan(0);
+                expect(Object.keys(res[0])).to.eql(["case_list_id", "case_list_name", "case_list_description", "cancer_study_id", "case_ids"]);
+                done();
+            });
+        });
+    });
+
 });
