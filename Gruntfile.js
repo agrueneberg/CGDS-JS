3fb2a66ee467a1e04a1959a324a3807b36ca6af5
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2013-07-24 17:46:55 -0500

    Use Grunt to run uglify after browserify.

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,11 +10,21 @@ module.exports = function (grunt) {
                     alias: "lib/<%= pkg.name %>.js:<%= pkg.name %>"
                 }
             }
+        },
+        uglify: {
+            dist: {
+                src: "dist/<%= pkg.name %>.js",
+                dest: "dist/<%= pkg.name %>.min.js"
+            },
+            options: {
+                banner: "/*! <%= pkg.name %> <%= pkg.version %> */\n"
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask("default", ["browserify"]);
+    grunt.registerTask("default", ["browserify", "uglify"]);
 
 };
