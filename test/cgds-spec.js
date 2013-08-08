fb9e51048d76cf48039063b374cdbc6597699880
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2013-08-08 12:22:23 -0500

    Improve grammar in tests.

--- test/cgds-spec.js
+++ test/cgds-spec.js
@@ -47,7 +47,7 @@ describe("CDGS-JS", function () {
             });
             it("should return a JSON representation of a tab-delimited file with two columns", function (done) {
                 cgds.getTypesOfCancer(function (err, res) {
-                    expect(res).to.be.a(Array);
+                    expect(res).to.be.an(Array);
                     expect(res.length).to.be.greaterThan(0);
                     expect(Object.keys(res[0])).to.eql(["type_of_cancer_id", "name"]);
                     done();
@@ -66,7 +66,7 @@ describe("CDGS-JS", function () {
             });
             it("should return a JSON representation of a tab-delimited file with three columns", function (done) {
                 cgds.getCancerStudies(function (err, res) {
-                    expect(res).to.be.a(Array);
+                    expect(res).to.be.an(Array);
                     expect(res.length).to.be.greaterThan(0);
                     expect(Object.keys(res[0])).to.eql(["cancer_study_id", "name", "description"]);
                     done();
@@ -93,7 +93,7 @@ describe("CDGS-JS", function () {
             });
             it("should return a JSON representation of a tab-delimited file with six columns", function (done) {
                 cgds.getGeneticProfiles("brca_tcga", function (err, res) {
-                    expect(res).to.be.a(Array);
+                    expect(res).to.be.an(Array);
                     expect(res.length).to.be.greaterThan(0);
                     expect(Object.keys(res[0])).to.eql(["genetic_profile_id", "genetic_profile_name", "genetic_profile_description", "cancer_study_id", "genetic_alteration_type", "show_profile_in_analysis_tab"]);
                     done();
@@ -120,7 +120,7 @@ describe("CDGS-JS", function () {
             });
             it("should return a JSON representation of a tab-delimited file with five columns", function (done) {
                 cgds.getCaseLists("brca_tcga", function (err, res) {
-                    expect(res).to.be.a(Array);
+                    expect(res).to.be.an(Array);
                     expect(res.length).to.be.greaterThan(0);
                     expect(Object.keys(res[0])).to.eql(["case_list_id", "case_list_name", "case_list_description", "cancer_study_id", "case_ids"]);
                     done();
@@ -167,7 +167,7 @@ describe("CDGS-JS", function () {
                     expect(res[0].case_list_id).to.be("brca_tcga_3way_complete");
                     numPatients = res[0].case_ids.trim().split(" ").length;
                     cgds.getProfileData("brca_tcga_3way_complete", "brca_tcga_mutations", "TP53", function (err, res) {
-                        expect(res).to.be.a(Array);
+                        expect(res).to.be.an(Array);
                         expect(res.length).to.be.greaterThan(0);
                         expect(Object.keys(res[0])).to.contain("GENE_ID");
                         expect(Object.keys(res[0])).to.contain("COMMON");
@@ -182,7 +182,7 @@ describe("CDGS-JS", function () {
                     expect(res[0].case_list_id).to.be("brca_tcga_3way_complete");
                     numPatients = res[0].case_ids.trim().split(" ").length;
                     cgds.getProfileData("brca_tcga_3way_complete", "brca_tcga_mutations", ["TP53", "GATA3"], function (err, res) {
-                        expect(res).to.be.a(Array);
+                        expect(res).to.be.an(Array);
                         expect(res.length).to.be.greaterThan(0);
                         expect(Object.keys(res[0])).to.contain("GENE_ID");
                         expect(Object.keys(res[0])).to.contain("COMMON");
@@ -197,7 +197,7 @@ describe("CDGS-JS", function () {
                     expect(res[0].case_list_id).to.be("brca_tcga_3way_complete");
                     numPatients = res[0].case_ids.trim().split(" ").length;
                     cgds.getProfileData("brca_tcga_3way_complete", ["brca_tcga_mutations", "brca_tcga_log2CNA"], "TP53", function (err, res) {
-                        expect(res).to.be.a(Array);
+                        expect(res).to.be.an(Array);
                         expect(res.length).to.be.greaterThan(0);
                         expect(Object.keys(res[0])).to.contain("GENETIC_PROFILE_ID");
                         expect(Object.keys(res[0])).to.contain("ALTERATION_TYPE");
@@ -246,7 +246,7 @@ describe("CDGS-JS", function () {
             });
             it("should return a JSON representation of a tab-delimited file with 18 columns when requesting a single gene and a single genetic profile", function (done) {
                 cgds.getMutationData("brca_tcga_mutations", "TP53", function (err, res) {
-                    expect(res).to.be.a(Array);
+                    expect(res).to.be.an(Array);
                     expect(res.length).to.be.greaterThan(0);
                     expect(Object.keys(res[0])).to.eql(["entrez_gene_id", "gene_symbol", "case_id", "sequencing_center", "mutation_status", "mutation_type", "validation_status", "amino_acid_change", "functional_impact_score", "xvar_link", "xvar_link_pdb", "xvar_link_msa", "chr", "start_position", "end_position", "reference_allele", "variant_allele", "genetic_profile_id"]);
                     done();
@@ -254,7 +254,7 @@ describe("CDGS-JS", function () {
             });
             it("should return a JSON representation of a tab-delimited file with 18 columns when requesting more than one gene and a single genetic profile", function (done) {
                 cgds.getMutationData("brca_tcga_mutations", ["TP53", "GATA3"], function (err, res) {
-                    expect(res).to.be.a(Array);
+                    expect(res).to.be.an(Array);
                     expect(res.length).to.be.greaterThan(0);
                     expect(Object.keys(res[0])).to.eql(["entrez_gene_id", "gene_symbol", "case_id", "sequencing_center", "mutation_status", "mutation_type", "validation_status", "amino_acid_change", "functional_impact_score", "xvar_link", "xvar_link_pdb", "xvar_link_msa", "chr", "start_position", "end_position", "reference_allele", "variant_allele", "genetic_profile_id"]);
                     done();
@@ -262,7 +262,7 @@ describe("CDGS-JS", function () {
             });
             it("should return a JSON representation of a tab-delimited file with 18 columns when requesting a single gene and more than one genetic profile", function (done) {
                 cgds.getMutationData(["brca_tcga_mutations", "brca_tcga_log2CNA"], "TP53", function (err, res) {
-                    expect(res).to.be.a(Array);
+                    expect(res).to.be.an(Array);
                     expect(res.length).to.be.greaterThan(0);
                     expect(Object.keys(res[0])).to.eql(["entrez_gene_id", "gene_symbol", "case_id", "sequencing_center", "mutation_status", "mutation_type", "validation_status", "amino_acid_change", "functional_impact_score", "xvar_link", "xvar_link_pdb", "xvar_link_msa", "chr", "start_position", "end_position", "reference_allele", "variant_allele", "genetic_profile_id"]);
                     done();
@@ -270,7 +270,7 @@ describe("CDGS-JS", function () {
             });
             it("should return a JSON representation of a tab-delimited file with 18 columns when requesting more than one gene and more than one genetic profile", function (done) {
                 cgds.getMutationData(["brca_tcga_mutations", "brca_tcga_log2CNA"], ["TP53", "GATA3"], function (err, res) {
-                    expect(res).to.be.a(Array);
+                    expect(res).to.be.an(Array);
                     expect(res.length).to.be.greaterThan(0);
                     expect(Object.keys(res[0])).to.eql(["entrez_gene_id", "gene_symbol", "case_id", "sequencing_center", "mutation_status", "mutation_type", "validation_status", "amino_acid_change", "functional_impact_score", "xvar_link", "xvar_link_pdb", "xvar_link_msa", "chr", "start_position", "end_position", "reference_allele", "variant_allele", "genetic_profile_id"]);
                     done();
@@ -278,7 +278,7 @@ describe("CDGS-JS", function () {
             });
             it("should return a JSON representation of a tab-delimited file with 18 columns when requesting a single gene, a single genetic profile, and a case list", function (done) {
                 cgds.getMutationData("brca_tcga_mutations", "TP53", "brca_tcga_3way_complete", function (err, res) {
-                    expect(res).to.be.a(Array);
+                    expect(res).to.be.an(Array);
                     expect(res.length).to.be.greaterThan(0);
                     expect(Object.keys(res[0])).to.eql(["entrez_gene_id", "gene_symbol", "case_id", "sequencing_center", "mutation_status", "mutation_type", "validation_status", "amino_acid_change", "functional_impact_score", "xvar_link", "xvar_link_pdb", "xvar_link_msa", "chr", "start_position", "end_position", "reference_allele", "variant_allele", "genetic_profile_id"]);
                     done();
@@ -305,7 +305,7 @@ describe("CDGS-JS", function () {
             });
             it("should return a JSON representation of a tab-delimited file with six columns", function (done) {
                 cgds.getClinicalData("brca_tcga_3way_complete", function (err, res) {
-                    expect(res).to.be.a(Array);
+                    expect(res).to.be.an(Array);
                     expect(res.length).to.be.greaterThan(0);
                     expect(Object.keys(res[0])).to.eql(["case_id", "overall_survival_months", "overall_survival_status", "disease_free_survival_months", "disease_free_survival_status", "age_at_diagnosis"]);
                     done();
