6f58fe5e7b277db3522a501c09a31b0a33393e80
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2013-08-08 13:21:59 -0500

    Catch errors if non-existing parameters are provided.

--- test/cgds-spec.js
+++ test/cgds-spec.js
@@ -47,6 +47,7 @@ describe("CDGS-JS", function () {
             });
             it("should return a JSON representation of a tab-delimited file with two columns", function (done) {
                 cgds.getTypesOfCancer(function (err, res) {
+                    expect(err).to.be(null);
                     expect(res).to.be.an(Array);
                     expect(res.length).to.be.greaterThan(0);
                     expect(Object.keys(res[0])).to.eql(["type_of_cancer_id", "name"]);
@@ -66,6 +67,7 @@ describe("CDGS-JS", function () {
             });
             it("should return a JSON representation of a tab-delimited file with three columns", function (done) {
                 cgds.getCancerStudies(function (err, res) {
+                    expect(err).to.be(null);
                     expect(res).to.be.an(Array);
                     expect(res.length).to.be.greaterThan(0);
                     expect(Object.keys(res[0])).to.eql(["cancer_study_id", "name", "description"]);
@@ -91,11 +93,20 @@ describe("CDGS-JS", function () {
                     expect(e.message).to.be("Please provide a callback parameter.");
                 });
             });
-            it("should return a JSON representation of a tab-delimited file with six columns", function (done) {
+            it("should return a JSON representation of a tab-delimited file with six columns for an existing cancer study", function (done) {
                 cgds.getGeneticProfiles("brca_tcga", function (err, res) {
+                    expect(err).to.be(null);
                     expect(res).to.be.an(Array);
                     expect(res.length).to.be.greaterThan(0);
                     expect(Object.keys(res[0])).to.eql(["genetic_profile_id", "genetic_profile_name", "genetic_profile_description", "cancer_study_id", "genetic_alteration_type", "show_profile_in_analysis_tab"]);
+                    done();
+                });
+            });
+            it("should return an error if the cancer study does not exist", function (done) {
+                cgds.getGeneticProfiles("foo", function (err, res) {
+                    expect(err).to.not.be(null);
+                    expect(err.message).to.be("Error: Problem when identifying a cancer study for the request.");
+                    expect(res).to.be(null);
                     done();
                 });
             });
@@ -118,11 +129,20 @@ describe("CDGS-JS", function () {
                     expect(e.message).to.be("Please provide a callback parameter.");
                 });
             });
-            it("should return a JSON representation of a tab-delimited file with five columns", function (done) {
+            it("should return a JSON representation of a tab-delimited file with five columns if cancer study exists", function (done) {
                 cgds.getCaseLists("brca_tcga", function (err, res) {
+                    expect(err).to.be(null);
                     expect(res).to.be.an(Array);
                     expect(res.length).to.be.greaterThan(0);
                     expect(Object.keys(res[0])).to.eql(["case_list_id", "case_list_name", "case_list_description", "cancer_study_id", "case_ids"]);
+                    done();
+                });
+            });
+            it("should return an error if the cancer study does not exist", function (done) {
+                cgds.getGeneticProfiles("foo", function (err, res) {
+                    expect(err).to.not.be(null);
+                    expect(err.message).to.be("Error: Problem when identifying a cancer study for the request.");
+                    expect(res).to.be(null);
                     done();
                 });
             });
@@ -167,6 +187,7 @@ describe("CDGS-JS", function () {
                     expect(res[0].case_list_id).to.be("brca_tcga_3way_complete");
                     numPatients = res[0].case_ids.trim().split(" ").length;
                     cgds.getProfileData("brca_tcga_3way_complete", "brca_tcga_mutations", "TP53", function (err, res) {
+                        expect(err).to.be(null);
                         expect(res).to.be.an(Array);
                         expect(res.length).to.be.greaterThan(0);
                         expect(Object.keys(res[0])).to.contain("GENE_ID");
@@ -182,6 +203,7 @@ describe("CDGS-JS", function () {
                     expect(res[0].case_list_id).to.be("brca_tcga_3way_complete");
                     numPatients = res[0].case_ids.trim().split(" ").length;
                     cgds.getProfileData("brca_tcga_3way_complete", "brca_tcga_mutations", ["TP53", "GATA3"], function (err, res) {
+                        expect(err).to.be(null);
                         expect(res).to.be.an(Array);
                         expect(res.length).to.be.greaterThan(0);
                         expect(Object.keys(res[0])).to.contain("GENE_ID");
@@ -197,6 +219,7 @@ describe("CDGS-JS", function () {
                     expect(res[0].case_list_id).to.be("brca_tcga_3way_complete");
                     numPatients = res[0].case_ids.trim().split(" ").length;
                     cgds.getProfileData("brca_tcga_3way_complete", ["brca_tcga_mutations", "brca_tcga_log2CNA"], "TP53", function (err, res) {
+                        expect(err).to.be(null);
                         expect(res).to.be.an(Array);
                         expect(res.length).to.be.greaterThan(0);
                         expect(Object.keys(res[0])).to.contain("GENETIC_PROFILE_ID");
@@ -216,7 +239,30 @@ describe("CDGS-JS", function () {
                     expect(e.message).to.be("You can specify multiple genes or multiple genetic profiles, but not both at once!");
                 });
             });
-
+            it("should return an error if the case set does not exist", function (done) {
+                cgds.getProfileData("foo", "brca_tcga_mutations", "TP53", function (err, res) {
+                    expect(err).to.not.be(null);
+                    expect(err.message).to.be("Error: Invalid case_set_id:  foo.");
+                    expect(res).to.be(null);
+                    done();
+                });
+            });
+            it("should return an error if the genetic profile does not exist", function (done) {
+                cgds.getProfileData("brca_tcga_3way_complete", "foo", "TP53", function (err, res) {
+                    expect(err).to.not.be(null);
+                    expect(err.message).to.be("No genetic profile available for genetic_profile_id:  foo.");
+                    expect(res).to.be(null);
+                    done();
+                });
+            });
+            it("should return an empty result if the gene does not exist", function (done) {
+                cgds.getProfileData("brca_tcga_3way_complete", "brca_tcga_mutations", "foo", function (err, res) {
+                    expect(err).to.be(null);
+                    expect(res).to.be.an(Array);
+                    expect(res.length).to.be(0);
+                    done();
+                });
+            });
         });
 
         describe("getMutationData", function () {
@@ -246,6 +292,7 @@ describe("CDGS-JS", function () {
             });
             it("should return a JSON representation of a tab-delimited file with 18 columns when requesting a single gene and a single genetic profile", function (done) {
                 cgds.getMutationData("brca_tcga_mutations", "TP53", function (err, res) {
+                    expect(err).to.be(null);
                     expect(res).to.be.an(Array);
                     expect(res.length).to.be.greaterThan(0);
                     expect(Object.keys(res[0])).to.eql(["entrez_gene_id", "gene_symbol", "case_id", "sequencing_center", "mutation_status", "mutation_type", "validation_status", "amino_acid_change", "functional_impact_score", "xvar_link", "xvar_link_pdb", "xvar_link_msa", "chr", "start_position", "end_position", "reference_allele", "variant_allele", "genetic_profile_id"]);
@@ -254,6 +301,7 @@ describe("CDGS-JS", function () {
             });
             it("should return a JSON representation of a tab-delimited file with 18 columns when requesting more than one gene and a single genetic profile", function (done) {
                 cgds.getMutationData("brca_tcga_mutations", ["TP53", "GATA3"], function (err, res) {
+                    expect(err).to.be(null);
                     expect(res).to.be.an(Array);
                     expect(res.length).to.be.greaterThan(0);
                     expect(Object.keys(res[0])).to.eql(["entrez_gene_id", "gene_symbol", "case_id", "sequencing_center", "mutation_status", "mutation_type", "validation_status", "amino_acid_change", "functional_impact_score", "xvar_link", "xvar_link_pdb", "xvar_link_msa", "chr", "start_position", "end_position", "reference_allele", "variant_allele", "genetic_profile_id"]);
@@ -262,6 +310,7 @@ describe("CDGS-JS", function () {
             });
             it("should return a JSON representation of a tab-delimited file with 18 columns when requesting a single gene and more than one genetic profile", function (done) {
                 cgds.getMutationData(["brca_tcga_mutations", "brca_tcga_log2CNA"], "TP53", function (err, res) {
+                    expect(err).to.be(null);
                     expect(res).to.be.an(Array);
                     expect(res.length).to.be.greaterThan(0);
                     expect(Object.keys(res[0])).to.eql(["entrez_gene_id", "gene_symbol", "case_id", "sequencing_center", "mutation_status", "mutation_type", "validation_status", "amino_acid_change", "functional_impact_score", "xvar_link", "xvar_link_pdb", "xvar_link_msa", "chr", "start_position", "end_position", "reference_allele", "variant_allele", "genetic_profile_id"]);
@@ -270,6 +319,7 @@ describe("CDGS-JS", function () {
             });
             it("should return a JSON representation of a tab-delimited file with 18 columns when requesting more than one gene and more than one genetic profile", function (done) {
                 cgds.getMutationData(["brca_tcga_mutations", "brca_tcga_log2CNA"], ["TP53", "GATA3"], function (err, res) {
+                    expect(err).to.be(null);
                     expect(res).to.be.an(Array);
                     expect(res.length).to.be.greaterThan(0);
                     expect(Object.keys(res[0])).to.eql(["entrez_gene_id", "gene_symbol", "case_id", "sequencing_center", "mutation_status", "mutation_type", "validation_status", "amino_acid_change", "functional_impact_score", "xvar_link", "xvar_link_pdb", "xvar_link_msa", "chr", "start_position", "end_position", "reference_allele", "variant_allele", "genetic_profile_id"]);
@@ -278,9 +328,26 @@ describe("CDGS-JS", function () {
             });
             it("should return a JSON representation of a tab-delimited file with 18 columns when requesting a single gene, a single genetic profile, and a case list", function (done) {
                 cgds.getMutationData("brca_tcga_mutations", "TP53", "brca_tcga_3way_complete", function (err, res) {
+                    expect(err).to.be(null);
                     expect(res).to.be.an(Array);
                     expect(res.length).to.be.greaterThan(0);
                     expect(Object.keys(res[0])).to.eql(["entrez_gene_id", "gene_symbol", "case_id", "sequencing_center", "mutation_status", "mutation_type", "validation_status", "amino_acid_change", "functional_impact_score", "xvar_link", "xvar_link_pdb", "xvar_link_msa", "chr", "start_position", "end_position", "reference_allele", "variant_allele", "genetic_profile_id"]);
+                    done();
+                });
+            });
+            it("should return an error if the genetic profile id does not exist", function (done) {
+                cgds.getMutationData("foo", "TP53", function (err, res) {
+                    expect(err).to.not.be(null);
+                    expect(err.message).to.be("Error: Problem when identifying a cancer study for the request.");
+                    expect(res).to.be(null);
+                    done();
+                });
+            });
+            it("should return an empty result if the gene does not exist", function (done) {
+                cgds.getMutationData("brca_tcga_mutations", "foo", function (err, res) {
+                    expect(err).to.be(null);
+                    expect(res).to.be.an(Array);
+                    expect(res.length).to.be(0);
                     done();
                 });
             });
@@ -308,6 +375,14 @@ describe("CDGS-JS", function () {
                     expect(res).to.be.an(Array);
                     expect(res.length).to.be.greaterThan(0);
                     expect(Object.keys(res[0])).to.eql(["case_id", "overall_survival_months", "overall_survival_status", "disease_free_survival_months", "disease_free_survival_status", "age_at_diagnosis"]);
+                    done();
+                });
+            });
+            it("should return an error if the case set does not exist", function (done) {
+                cgds.getClinicalData("foo", function (err, res) {
+                    expect(err).to.not.be(null);
+                    expect(err.message).to.be("Error: Problem when identifying a cancer study for the request.");
+                    expect(res).to.be(null);
                     done();
                 });
             });
